b4ab5d66b49abc8d0811221b6f7c73efc13121f2
Mari Angeles <mariangeles.mutor@gmail.com>
2025-02-04 20:07:58 +0100

    Initial filters on the gateway

---
+++ src/main/java/com/desarrollo/laboratorio/api_gateway/decorator/RequestDecoratorFactory.java
@@ -0,0 +1,23 @@
+package com.desarrollo.laboratorio.api_gateway.decorator;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.desarrollo.laboratorio.api_gateway.model.GatewayRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.server.reactive.ServerHttpRequestDecorator;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@RequiredArgsConstructor
+public class RequestDecoratorFactory {
+
+    private final ObjectMapper objectMapper;
+
+    public ServerHttpRequestDecorator getDecorator(GatewayRequest request) {
+        return switch (request.getTargetMethod().name().toUpperCase()) {
+            case "GET" -> new GetRequestDecorator(request);
+            case "POST" -> new PostRequestDecorator(request, objectMapper);
+            default -> throw new IllegalArgumentException("Invalid http method");
+        };
+    }
+}
